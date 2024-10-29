f2b334b81a89bdfdb8a384cb64f279c2a1d6fd9c
Iori Yanokura <ab.ioryz@gmail.com>
2024-10-29 20:54:07 +0900

    Use ruff

--- tests/urdfeus_tests/test_console_scripts.py
+++ tests/urdfeus_tests/test_console_scripts.py
@@ -6,7 +6,6 @@ import unittest
 
 from skrobot.data import fetch_urdfpath
 
-
 data_dir = osp.abspath(osp.dirname(__file__))
 
 
@@ -19,40 +18,36 @@ def run_command(cmd):
 
 
 class TestConsoleScripts(unittest.TestCase):
-
     tmp_output = None
     urdfpath = None
 
     @classmethod
     def setUpClass(cls):
         cls.tmp_output = tempfile.TemporaryDirectory()
-        os.environ['SKROBOT_CACHE_DIR'] = cls.tmp_output.name
+        os.environ["SKROBOT_CACHE_DIR"] = cls.tmp_output.name
         cls.urdfpath = fetch_urdfpath()
 
     def test_mesh2eus(self):
-        target_mesh = osp.join(osp.dirname(self.urdfpath), 'meshes',
-                               'base_link.dae')
-        output_eus_path = osp.join(osp.dirname(self.urdfpath), 'meshes',
-                                   'base_link.l')
+        target_mesh = osp.join(osp.dirname(self.urdfpath), "meshes", "base_link.dae")
+        output_eus_path = osp.join(osp.dirname(self.urdfpath), "meshes", "base_link.l")
 
         cmds = [
-            f'mesh2eus {target_mesh} {output_eus_path}',
-            f'mesh2eus {target_mesh} {output_eus_path} --voxel-size 0.001',
+            f"mesh2eus {target_mesh} {output_eus_path}",
+            f"mesh2eus {target_mesh} {output_eus_path} --voxel-size 0.001",
         ]
         for cmd in cmds:
             result = run_command(cmd)
             assert result.returncode == 0
 
     def test_urdf2eus(self):
-        output_eus_path = osp.join(
-            osp.dirname(self.urdfpath), 'fetch.l')
-        yaml_path = osp.join(data_dir, 'fetch.yaml')
+        output_eus_path = osp.join(osp.dirname(self.urdfpath), "fetch.l")
+        yaml_path = osp.join(data_dir, "fetch.yaml")
 
         cmds = [
-            f'urdf2eus {self.urdfpath} {output_eus_path}',
-            f'urdf2eus {self.urdfpath} {output_eus_path} --voxel-size 0.001',
-            f'urdf2eus {self.urdfpath} {output_eus_path}'
-            f' --voxel-size 0.001 --yaml-path {yaml_path}',
+            f"urdf2eus {self.urdfpath} {output_eus_path}",
+            f"urdf2eus {self.urdfpath} {output_eus_path} --voxel-size 0.001",
+            f"urdf2eus {self.urdfpath} {output_eus_path}"
+            + f" --voxel-size 0.001 --yaml-path {yaml_path}",
         ]
         for cmd in cmds:
             result = run_command(cmd)
