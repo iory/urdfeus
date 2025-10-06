919e9bb78aecd60dab4f0388b1ee1c554d286e68
Iori Yanokura <ab.ioryz@gmail.com>
2025-10-06 22:05:42 +0900

    Use importlib.metadata instead of pkg_resources for Python 3.7+

--- urdfeus/__init__.py
+++ urdfeus/__init__.py
@@ -1,6 +1,20 @@
 # flake8: noqa
 
-import pkg_resources
+import sys
 
 
-__version__ = pkg_resources.get_distribution("urdfeus").version
+if (sys.version_info[0] == 3 and sys.version_info[1] >= 7) \
+    or sys.version_info[0] > 3:
+    import importlib.metadata
+
+    def determine_version(module_name):
+        return importlib.metadata.version(module_name)
+
+    __version__ = determine_version('urdfeus')
+else:
+    import pkg_resources
+
+    def determine_version(module_name):
+        return pkg_resources.get_distribution(module_name).version
+
+    __version__ = determine_version('urdfeus')
